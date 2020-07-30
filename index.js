c320f43b9802bf4533d45da719cd6723f88d22eb
sarah rose <xsarahrosemeow@gmail.com>
2020-07-30 17:35:13 -0400

    completed

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ function removeFlavorByName(arrayName, flavorToRemove) {
   return arrayName;
 }
 removeFlavorByName(originalFlavors, "Banana Nut Fudge");
-console.log(`after splice ${originalFlavors}`);
+console.log(`the answer to task 5 is ${originalFlavors}`);
 
 /* Task 6: With all of these changes going on, we don't want to lose track of the actual, original 31 flavors. Write a function called copy that makes a copy of the array. 
 
@@ -138,9 +138,14 @@ Your function should accept:
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
 
-function copy(/*code here*/) {
-  /*code here*/
+const copyOriginal = [];
+function copyArray(newArray, originalArray) {
+  newArray = [...originalArray];
+  console.log(newArray);
+  return newArray;
 }
+
+console.log(copyArray(copyOriginal, originalFlavors));
 
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
@@ -157,9 +162,19 @@ DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 
 hint - you can use the .includes method to help you solve this */
 
-function filterByWord(/*code here*/) {
-  /*code here*/
+function filterByWord(inputArray, flavorString) {
+  flavorArray = [];
+  for (i = 0; i < inputArray.length; i++) {
+    if (inputArray[i].includes(flavorString)) {
+      flavorArray.push(inputArray[i]);
+    }
+  }
+  return flavorArray;
 }
+
+console.log(
+  "the answer to task 7 is " + filterByWord(originalFlavors, "Peppermint")
+);
 
 /* 🧁🍦🍨 STRETCH 🍨🍦🍫*/
 
